af075e51d9a71c8bb5337a29193d9d36d29bbb1b
Theng <tyang13@alaska.edu>
2020-03-14 14:06:07 -0800

    added test 4- test for larger vector, test failed

--- HW2_TTD/test.cpp
+++ HW2_TTD/test.cpp
@@ -11,6 +11,7 @@
 using std::vector;
 #include<string>
 using std::string;
+using std::to_string;
 
 TEST_CASE("Return a vector") {
 	INFO("Function must return a empty vector");
@@ -22,5 +23,14 @@ TEST_CASE("Return a vector") {
 	INFO("Function must return a vector with string '1,''2,''3,''4,'5' ");
 	REQUIRE(fizzBuzz(5) == vector<string>{"1,", "2,", "3,", "4,", "5"});
 
-
+	INFO("Function must return a vector of 50 string number: '1,',...'50' ");
+	REQUIRE(fizzBuzz(50) == vector<string>{ "1,", "2,", "3,", "4,", "5,", "6,",
+											"7,", "8,", "9,", "10,", "11,", "12,",
+											"13,", "14,", "15,", "16,", "17,", "18,",
+											"19,", "20,", "21,", "22,", "23,","24,",
+											"25,", "26,", "27,", "28,", "29,", "30,", 
+											"31,", "32,", "33,", "34,","35,", "36,", 
+											"37,", "38,", "39,", "40,", "41,", "42,", 
+											"43,", "44,", "45,","46,", "47,", "48,", 
+											"49,", "50" });
 }
