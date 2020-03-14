0b1d543691d1e8c51fde43a322252aefbae4cbed
Theng <tyang13@alaska.edu>
2020-03-14 15:11:26 -0800

    add test 8- all divisible by 3 is fizz (passed), add {} on each test

--- HW2_TTD/test.cpp
+++ HW2_TTD/test.cpp
@@ -16,52 +16,88 @@ using std::to_string;
 
 TEST_CASE("Return a vector generator") {
 
-	INFO("Function must return a empty vector");
-	REQUIRE(fizzBuzz(0,false,false) == vector<string>{});
+	{
+		INFO("Function must return a empty vector");
+		REQUIRE(fizzBuzz(0, false, false) == vector<string>{});
+	}
 
-	INFO("Function must return a vector with the string '1'");
-	REQUIRE(fizzBuzz(1,false, false) == vector<string>{"1"});
+	{
+		INFO("Function must return a vector with the string '1'");
+		REQUIRE(fizzBuzz(1, false, false) == vector<string>{"1"});
+	}
 
-	INFO("Function must return a vector with string '1,''2,''3,''4,'5' ");
-	REQUIRE(fizzBuzz(5,false,false) == vector<string>{"1,", "2,", "3,", "4,", "5"});
+	{
+		INFO("Function must return a vector with string '1,''2,''3,''4,'5' ");
+		REQUIRE(fizzBuzz(5, false, false) == vector<string>{"1,", "2,", "3,", "4,", "5"});
+	}
 
-	INFO("Function must return a vector of 50 string number: '1,',...'50' ");
-	REQUIRE(fizzBuzz(50,false,false) == vector<string>{ "1,", "2,", "3,", "4,", "5,", "6,",
-											"7,", "8,", "9,", "10,", "11,", "12,",
-											"13,", "14,", "15,", "16,", "17,", "18,",
-											"19,", "20,", "21,", "22,", "23,","24,",
-											"25,", "26,", "27,", "28,", "29,", "30,", 
-											"31,", "32,", "33,", "34,","35,", "36,", 
-											"37,", "38,", "39,", "40,", "41,", "42,", 
-											"43,", "44,", "45,","46,", "47,", "48,", 
-											"49,", "50"                               });
+	{
+		INFO("Function must return a vector of 50 string number: '1,',...'50' ");
+		REQUIRE(fizzBuzz(50, false, false) == vector<string>{ "1,", "2,", "3,", "4,", "5,", "6,",
+															  "7,", "8,", "9,", "10,", "11,", "12,",
+															  "13,", "14,", "15,", "16,", "17,", "18,",
+															  "19,", "20,", "21,", "22,", "23,", "24,",
+														   	  "25,", "26,", "27,", "28,", "29,", "30,",
+															  "31,", "32,", "33,", "34,", "35,", "36,",
+															  "37,", "38,", "39,", "40,", "41,", "42,",
+															  "43,", "44,", "45,", "46,", "47,", "48,",
+															  "49,", "50"                               });
+	}
 
-	INFO("Function must return a generated vector of 100 string number");
-	REQUIRE(fizzBuzz(100,false,false) == vector<string> { "1,", "2,", "3,", "4,", "5,", "6,", 
-											  "7,", "8,", "9,", "10,", "11,", "12,",
-											  "13,", "14,", "15,", "16,", "17,", "18,", 
-											  "19,", "20,", "21,", "22,", "23,","24,", 
-											  "25,", "26,", "27,", "28,", "29,", "30,", 
-											  "31,", "32,", "33,", "34,","35,", "36,", 
-											  "37,", "38,", "39,", "40,", "41,", "42,", 
-											  "43,", "44,", "45,","46,", "47,", "48,",
-											  "49,", "50,", "51,", "52,", "53,", "54,", 
-											  "55,", "56,","57,", "58,", "59,", "60,", 
-											  "61,", "62,", "63,", "64,", "65,", "66,", 
-											  "67,","68,", "69,", "70,", "71,", "72,", 
-											  "73,", "74,", "75,", "76,", "77,", "78,",
-											  "79,", "80,", "81,", "82,", "83,", "84,", 
-											  "85,", "86,", "87,", "88,", "89,","90,", 
-											  "91,", "92,", "93,", "94,", "95,", "96,", 
-											  "97,", "98,", "99,", "100"               });
+	{
+		INFO("Function must return a generated vector of 100 string number");
+		REQUIRE(fizzBuzz(100, false, false) == vector<string> { "1,", "2,", "3,", "4,", "5,", "6,",
+																"7,", "8,", "9,", "10,", "11,", "12,",
+																"13,", "14,", "15,", "16,", "17,", "18,",
+																"19,", "20,", "21,", "22,", "23,", "24,",
+																"25,", "26,", "27,", "28,", "29,", "30,",
+																"31,", "32,", "33,", "34,", "35,", "36,",
+																"37,", "38,", "39,", "40,", "41,", "42,",
+																"43,", "44,", "45,", "46,", "47,", "48,",
+																"49,", "50,", "51,", "52,", "53,", "54,",
+																"55,", "56,", "57,", "58,", "59,", "60,",
+																"61,", "62,", "63,", "64,", "65,", "66,",
+																"67,", "68,", "69,", "70,", "71,", "72,",
+																"73,", "74,", "75,", "76,", "77,", "78,",
+																"79,", "80,", "81,", "82,", "83,", "84,",
+																"85,", "86,", "87,", "88,", "89,", "90,",
+																"91,", "92,", "93,", "94,", "95,", "96,",
+																"97,", "98,", "99,", "100"               });
+	}
 }
 
 TEST_CASE("Change every integer divisible by 3 to Fizz") {
 
-	INFO("'3' should be changed to fizz");
-	REQUIRE(fizzBuzz(3,true,false) == vector<string>{"1,", "2,", "fizz"});
+	{
+		INFO("'3' should be changed to fizz");
+		REQUIRE(fizzBuzz(3, true, false) == vector<string>{"1,", "2,", "fizz"});
+	}
 
-	INFO("'3', '6', '9', and '12' should be 'fizz'");
-	REQUIRE(fizzBuzz(12, true, false) == vector<string>{"1,", "2,", "fizz,", "4,", "5,", "fizz,",
-														"7,", "8,", "fizz,", "10,", "11,", "fizz"});
+	{
+		INFO("'3', '6', '9', and '12' should be 'fizz'");
+		REQUIRE(fizzBuzz(12, true, false) == vector<string>{"1,", "2,", "fizz,", "4,", "5,", "fizz,",
+			"7,", "8,", "fizz,", "10,", "11,", "fizz"});
+	}
+
+	{
+		INFO("All number divisible by 3 is 'fizz'");
+		REQUIRE(fizzBuzz(100, true, false) == vector<string>{ "1,", "2,", "fizz,", "4,", "5,", 
+															  "fizz,", "7,", "8,", "fizz,", "10,", 
+															  "11,", "fizz,", "13,", "14,", "fizz,", 
+															  "16,", "17,", "fizz,", "19,", "20,",
+															  "fizz,", "22,", "23,", "fizz,", "25,", 
+															  "26,", "fizz,", "28,", "29,", "fizz,",
+															  "31,", "32,", "fizz,", "34,", "35,", "fizz,",
+															  "37,", "38,", "fizz,", "40,","41,", "fizz,", 
+															  "43,", "44,", "fizz,", "46,", "47,", "fizz,", 
+															  "49,", "50,","fizz,", "52,", "53,", "fizz,", 
+															  "55,", "56,", "fizz,", "58,", "59,", "fizz,",
+															  "61,", "62,", "fizz,", "64,", "65,", "fizz,", 
+															  "67,", "68,", "fizz,", "70,","71,", "fizz,", 
+															  "73,", "74,", "fizz,", "76,", "77,", "fizz,", 
+															  "79,", "80,","fizz,", "82,", "83,", "fizz,", 
+															  "85,", "86,", "fizz,", "88,", "89,", "fizz,",
+															  "91,", "92,", "fizz,", "94,", "95,", "fizz,", 
+															  "97,", "98,", "fizz,", "100"                 });
+	}
 }
